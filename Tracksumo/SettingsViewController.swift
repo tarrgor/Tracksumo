68bbf4bc3f3dc4776b5a8d8464da82c89a7a6b12
Krystian Gontarek <krystian@gontarek.net>
2016-12-21 22:51:40 +0100

    XCode Crash Fixing

--- Tracksumo/SettingsViewController.swift
+++ Tracksumo/SettingsViewController.swift
@@ -8,7 +8,34 @@
 
 import UIKit
 
-class SettingsViewController : UIViewController, Pageable {
+class SettingsViewController : UIViewController, Pageable, UITableViewDataSource, UITableViewDelegate {
+    
+    // tableView connection
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        
+        // tableView config
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+    }
+    
+    // MARK: tableView number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // MARK: tableView cell content
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ActivityCell") as! UIActivityCell
+        cell.activityName.text = "XCode Configuration"
+        cell.activityCategory.text = "Coding"
+        
+        return cell
+    }
+    
     
     let pageIndex: Int = 0
     
---
+++ Tracksumo/UIActivityCell.swift
@@ -0,0 +1,16 @@
+//
+//  UIActivityCell.swift
+//  Tracksumo
+//
+//  Created by Krystian Gontarek on 20/12/2016.
+//  Copyright © 2016 Kreskow&Karrmarr DevHeroes. All rights reserved.
+//
+
+import UIKit
+
+class UIActivityCell : UITableViewCell {
+    
+    @IBOutlet weak var activityName: UILabel!
+    @IBOutlet weak var activityCategory: UILabel!
+    
+}
